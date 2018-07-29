2f76eb772f30a5b9046a26ae5aaec86036ebd149
yangpengcheng <yangpengcheng@lvmama.com>
2018-07-29 15:28:20 +0800

    提交相关代码20180729

--- driving-school-startup/src/main/java/org/driving/school/startup/Main.java
+++ driving-school-startup/src/main/java/org/driving/school/startup/Main.java
@@ -3,8 +3,10 @@ package org.driving.school.startup;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
+//@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(value="org.driving")
 @MapperScan("org.driving.school.dal.mapper")
